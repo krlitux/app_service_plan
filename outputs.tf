87f3681b6dc572ea0a3f77da925ba618b98b6b26
krlitux <krlitux@linux.es>
2021-01-16 13:18:06 -0500

    Version 1.0.0 del módulo aspl

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,13 @@ output "aspl_name" {
   description = "Mapa con los nombres de los App Service Plan aprovisionados"
   value = { for region in keys(azurerm_app_service_plan.aspl) : region => azurerm_app_service_plan.aspl[region].name }
 }
+
 output "aspl_tier" {
-  description = "Mapa con los nombres de los App Service Plan aprovisionados"
+  description = "Mapa con los niveles de los App Service Plan aprovisionados"
   value = local.aspl_tier
 }
+
 output "aspl_id" {
-  description = "Mapa con los nombres de los App Service Plan aprovisionados"
+  description = "Mapa con los IDs de los App Service Plan aprovisionados"
   value = { for region in keys(azurerm_app_service_plan.aspl) : region => azurerm_app_service_plan.aspl[region].id }
 }
